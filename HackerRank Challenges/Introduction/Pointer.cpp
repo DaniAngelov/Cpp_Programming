b39ac6dd9e004f96383c4e0781b9d94422906691
DaniAngelov <60110955+DaniAngelov@users.noreply.github.com>
2020-05-07 16:04:52 +0300

    Create Pointer.cpp

---
+++ HackerRank Challenges/Introduction/Pointer.cpp
@@ -0,0 +1,27 @@
+//  github.com/DaniAngelov
+
+#include <stdio.h>
+#include <iostream>
+using namespace std;
+
+void update(int *a,int *b) 
+{
+    
+    int sum = *a + *b ;
+    int diff = abs(*a - *b) ;
+
+    *a = sum;
+    *b = diff;  
+    
+}
+
+int main() {
+    int a, b;
+    int *pa = &a, *pb = &b;
+    
+    scanf("%d %d", &a, &b);
+    update(pa, pb);
+    printf("%d\n%d", a, b);
+
+    return 0;
+}
